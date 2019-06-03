c0a79b32c36b5fa1be05cd9de054dffa7f0ddc70
Jack Clackett <jclackett@gmail.com>
2019-06-03 15:52:36 +0200

    add hooks package

---
+++ packages/hooks/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./useDebounce"
+export * from "./useEventListener"
+export * from "./useForm"
+export * from "./useLocalStorage"
+export * from "./useOnClickOutside"
---
+++ packages/hooks/src/useEventListener.ts
@@ -0,0 +1,10 @@
+import { useEffect } from "react"
+
+function useEventListener(key: string, cb: (e: any) => void, options?: any) {
+  useEffect(() => {
+    window.addEventListener(key, cb, options)
+    return () => window.removeEventListener(key, cb, options)
+  }, [cb, key, options])
+}
+
+export default useEventListener
